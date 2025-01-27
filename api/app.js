da600517506d84ef528148346ea2078d163a84ad
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-01-27 12:57:43 +0000

    Complete api/topics endpoint

--- api/app.js
+++ api/app.js
@@ -1,8 +1,13 @@
 const { getEndpoints } = require("./controllers/main.controller");
-
+const topics = require('./controllers/topics.controller')
 const express = require('express')
 const app = express();
 
 app.get('/api', getEndpoints)
+app.get('/api/topics', topics.getTopics)
+
+app.use((err, request, response, next) => {
+    response.status(500).send(err)
+})
 
 module.exports = app
