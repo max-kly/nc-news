68070451a116cbb7f7ed98e4581d4cd245669b42
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-01-27 14:57:39 +0000

    Complete tests for api/articles/article_id endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -36,3 +36,40 @@ describe('/api/topics tests:', () => {
       })
   })
 })
+describe('/api/articles/:article_id tests:', () => {
+  test('GET 200: Returns an article with correct id', () => {
+    const output = {
+      article_id: 1,
+      title: "Living in the shadow of a great man",
+      topic: "mitch",
+      author: "butter_bridge",
+      body: "I find this existence challenging",
+      created_at: '2020-07-09T20:11:00.000Z',
+      votes: 100,
+      article_img_url:
+        "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+    }
+    return request(app)
+      .get('/api/articles/1')
+      .expect(200)
+      .then(({ body: { article } }) => {
+        expect(article).toEqual(output)
+      })
+  })
+  test('GET 404: Returns a "Not found" message when article with such id does not exist', () => {
+    return request(app)
+      .get('/api/articles/94949')
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe('Not found')
+      })
+  })
+  test('GET 400: Returns a "Bad request" message when passed invalid id', () => {
+    return request(app)
+      .get('/api/articles/myId')
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe('Bad request')
+      })
+  })
+})
