902e944181e9e2dd22100a5559301fc85ecc0195
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-01-27 18:17:29 +0000

    Complete api/articles endpoint

--- api/models/articles.model.js
+++ api/models/articles.model.js
@@ -1,13 +1,20 @@
 const db = require('../../db/connection')
 const format = require('pg-format')
+function fetchArticles() {
+    // return db.query('SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comment_id) AS comment_count FROM comments LEFT JOIN articles ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC')
+    return db.query('SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC')
+        .then(({ rows }) => {
+            return rows
+        })
+}
 function fetchArticleById(article_id) {
     const sql = format('SELECT * FROM articles WHERE article_id = %L', article_id)
     return db.query(sql)
         .then(({ rows }) => {
             if (!rows.length) {
-                return Promise.reject({msg: 'Not found'})
+                return Promise.reject({ msg: 'Not found' })
             }
             return rows[0]
         })
 }
-module.exports = { fetchArticleById }
+module.exports = { fetchArticles, fetchArticleById }
