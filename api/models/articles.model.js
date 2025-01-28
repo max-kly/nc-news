8da692ac1ca526c141ba498575cea9363d0d2a7b
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-01-28 08:39:10 +0000

    Remove commented SQL

--- api/models/articles.model.js
+++ api/models/articles.model.js
@@ -1,7 +1,6 @@
 const db = require('../../db/connection')
 const format = require('pg-format')
 function fetchArticles() {
-    // return db.query('SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comment_id) AS comment_count FROM comments LEFT JOIN articles ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC')
     return db.query('SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC')
         .then(({ rows }) => {
             return rows
