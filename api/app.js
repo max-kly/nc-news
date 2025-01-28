ddba67f029423e9b7d4ac3c39dd3886a40ff18cf
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-01-28 14:11:04 +0000

    Complete POST /api/articles/:article/comments including error handling + refactor MVC to format response and use additional models on a controller side

--- api/app.js
+++ api/app.js
@@ -4,15 +4,17 @@ const articles = require('./controllers/articles.controller')
 const comments = require('./controllers/comments.controller')
 const express = require('express')
 const app = express();
+app.use(express.json());
 
 app.get('/api', getEndpoints)
 app.get('/api/topics', topics.getTopics)
 app.get('/api/articles', articles.getArticles)
 app.get('/api/articles/:article_id', articles.getArticlesById)
 app.get('/api/articles/:article_id/comments', comments.getCommentsByArticleID)
+app.post('/api/articles/:article_id/comments', comments.postComment)
 
 app.use((err, request, response, next) => {
-    if (err.code === '22P02') {
+    if (err.code === '22P02' || err.code === '23503') {
         response.status(400).send({ msg: 'Bad request' })
     }
     else {
--- api/controllers/comments.controller.js
+++ api/controllers/comments.controller.js
@@ -1,13 +1,38 @@
-const { fetchCommentsByArticleID } = require("../models/comments.model")
+const { fetchArticleById } = require("../models/articles.model")
+const { fetchCommentsByArticleID, addComment } = require("../models/comments.model")
 
 function getCommentsByArticleID(request, response, next) {
     const { article_id } = request.params
-    fetchCommentsByArticleID(article_id)
+    fetchArticleById(article_id)
+    .then(() => {
+        fetchCommentsByArticleID(article_id)
         .then((comments) => {
-            response.status(200).send(comments)
+            response.status(200).send({comments})
+        })
+        .catch((err) => {
+            next(err)
+        })
+    })
+    .catch((err) => {
+        next(err)
+    })
+}
+function postComment(request, response, next) {
+    const { article_id } = request.params
+    const { username } = request.body
+    const { body } = request.body
+    fetchArticleById(article_id)
+        .then(() => {
+            addComment(article_id, username, body)
+                .then(({ rows }) => {
+                    response.status(201).send({ comment: rows[0] })
+                })
+                .catch((err) => {
+                    next(err)
+                })
         })
         .catch((err) => {
             next(err)
         })
 }
-module.exports = { getCommentsByArticleID }
+module.exports = { getCommentsByArticleID, postComment }
