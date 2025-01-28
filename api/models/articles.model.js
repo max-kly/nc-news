5cf03b8de7d19fa3b28546c30a554b22375c3212
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-01-28 19:42:52 +0000

    Complete sorting queries for /api/articles endpoint

--- api/models/articles.model.js
+++ api/models/articles.model.js
@@ -1,7 +1,10 @@
 const db = require('../../db/connection')
 const format = require('pg-format')
-function fetchArticles() {
-    return db.query('SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC')
+function fetchArticles(sort_by, order) {
+    const sqlOrder = order === undefined ? 'DESC' : order.toUpperCase()
+    const sqlSort = sort_by === undefined ? 'created_at' : sort_by
+    const sql = format('SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY %s %s', sqlSort, sqlOrder)
+    return db.query(sql)
         .then(({ rows }) => {
             return rows
         })
