ec50e4cf438b16df5af88827f8258eb74e5d7ac1
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-01-29 18:19:01 +0000

    Complete GET users/:username endpoint

--- api/routers/users.router.js
+++ api/routers/users.router.js
@@ -2,5 +2,6 @@ const controller = require('../controllers/users.controller')
 const userRouter = require('express').Router()
 
 userRouter.get('/', controller.getAllUsers)
+userRouter.get('/:username', controller.getUserByUsername)
 
 module.exports = userRouter
