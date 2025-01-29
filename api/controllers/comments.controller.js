5c7963579801276c851ef75287f7809fb34b3a8d
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-01-29 18:50:41 +0000

    Complete PATCH /comments/:comment_id including error handling

--- api/controllers/comments.controller.js
+++ api/controllers/comments.controller.js
@@ -1,4 +1,4 @@
-const { fetchCommentById, removeComment } = require("../models/comments.model")
+const { fetchCommentById, removeComment, changeCommentVotes } = require("../models/comments.model")
 
 function deleteComment(request, response, next) {
     const { comment_id } = request.params
@@ -16,5 +16,19 @@ function deleteComment(request, response, next) {
             next(err)
         })
 }
+function updateCommentVotes(request, response, next) {
+    const { comment_id } = request.params
+    const { inc_votes } = request.body
+    fetchCommentById(comment_id)
+        .then(() => {
+            return changeCommentVotes(comment_id, inc_votes)
+                .then((comment) => {
+                    response.status(200).send({ comment })
+                })
+        })
+        .catch((err) => {
+            next(err)
+        })
+}
 
-module.exports = { deleteComment }
+module.exports = { deleteComment, updateCommentVotes }
--- api/routers/comments.router.js
+++ api/routers/comments.router.js
@@ -1,6 +1,7 @@
 const controller = require('../controllers/comments.controller')
 const commentsRouter = require('express').Router()
 
+commentsRouter.patch('/:comment_id', controller.updateCommentVotes)
 commentsRouter.delete('/:comment_id', controller.deleteComment)
 
 module.exports = commentsRouter
