c8d065e628a5813d4df6bf070b1468815c52a9d6
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-01-30 16:32:16 +0000

    Complete POST /api/topics endpoint

--- api/models/topics.model.js
+++ api/models/topics.model.js
@@ -8,4 +8,19 @@ function fetchTopics() {
             return err
         })
 }
-module.exports = { fetchTopics }
+function addNewTopic(slug, description) {
+    if (!slug || !description) {
+        return Promise.reject({ status: 400, msg: 'Bad request: required fields are missing' })
+    }
+    return db.query('SELECT * FROM topics WHERE slug = $1', [slug])
+        .then(({ rows }) => {
+            if (rows.length > 0) {
+                return Promise.reject({ status: 409, msg: 'Topic already exists' })
+            }
+            return db.query('INSERT INTO topics (slug, description) VALUES ($1, $2) RETURNING *', [slug, description])
+                .then(({ rows }) => {
+                    return rows[0]
+                })
+        })
+}
+module.exports = { fetchTopics, addNewTopic }
--- api/routers/topics.router.js
+++ api/routers/topics.router.js
@@ -2,5 +2,6 @@ const topicsRouter = require('express').Router()
 const controller = require('../controllers/topics.controller')
 
 topicsRouter.get('/', controller.getTopics)
+topicsRouter.post('/', controller.postTopic)
 
 module.exports = topicsRouter
