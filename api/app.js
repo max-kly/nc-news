f5d8592481b3af48737cc65a4eba73977e5bd494
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-02-17 10:28:28 +0000

    Allow cross origin resource sharing

--- api/app.js
+++ api/app.js
@@ -5,7 +5,9 @@ const comments = require('./controllers/comments.controller')
 const users = require('./controllers/users.controller')
 const express = require('express');
 const apiRouter = require("./routers/api.router");
+const cors = require('cors')
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter)
